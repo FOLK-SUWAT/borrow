cf1698793a564126e7eb842907335ed95b1a597d
FOLKQQ <41606593+FOLKQQ@users.noreply.github.com>
2020-03-26 22:11:41 +0700

    Update configmaill.php

--- configmaill.php
+++ configmaill.php
@@ -1,21 +1,21 @@
 <?php
 
 /*
-$mail->Host = "ns161.naxza.com";
+$mail->Host = "";
 $mail->Port = 587;
 $mail->SMTPSecure = false;
 $mail->SMTPAutoTLS = false;
 $mail->SMTPAuth = true;
-$mail->Username = "booking.room@kice-center.com";
-$mail->Password = "Kice1234";*/
+$mail->Username = "";
+$mail->Password = "";*/
            
                     $mail->Host = "smtp.gmail.com";
                     $mail->Port = 587;
                     $mail->SMTPSecure = 'tls';
                     $mail->SMTPAutoTLS = false;
                     $mail->SMTPAuth = true;
-                    $mail->Username = "fbifolkcom2@gmail.com";
-                    $mail->Password = "Inori23001010";
+                    $mail->Username = "";
+                    $mail->Password = "";
 
   
 
